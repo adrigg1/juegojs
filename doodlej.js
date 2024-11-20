fb42ec8cccd9da6e452de4cd52bd30c1790ead8b
adriangarciagomez <1918790@alu.murciaeduca.es>
2024-11-20 10:22:48 +0100

    configuracion de variables y reorganizacion de carpetas

--- doodlej.js
+++ doodlej.js
@@ -1,29 +1,67 @@
-const canvas = document.getElementById('gameCanvas'); //Lienzo
-const ctx = canvas.getContext('2d');  //contexto
+//tablero
+let tablero;
+let anchoTablero = 360;
+let altoTablero = 576;
+let contexto;
 
-//configuración básica
-const canvasWidth = canvas.width;
-const canvasHeight = canvas.height;
+//doodler
+let anchoDoodler = 46;
+let altoDoodler = 46;
+let doodlerX = anchoTablero / 2 - anchoDoodler / 2;
+let doodlerY = altoTablero * 7 / 8 - altoDoodler;
+let doodlerImgDerecha;
+let doodlerImgIzquierda;
 
-const backgroundSprite = new Image();
-backgroundSprite.src = 'assets/imagenes/bg_layer3.png';
-
-// Función para dibujar el fondo en el canvas
-function drawBackground() {
-    ctx.drawImage(backgroundSprite, 0, 0, canvas.width, canvas.height);
+let doodler = {
+    img: null,
+    x: doodlerX,
+    y: doodlerY,
+    ancho: anchoDoodler,
+    alto: altoDoodler
 }
 
-// Bucle principal del juego
-function gameLoop() {
-    // Dibujar el fondo en cada cuadro
-    drawBackground();
+//físicas
+let velocidadX = 0; 
+let velocidadY = 0; //velocidad del salto del doodler
+let velocidadInicialY = -8; //velocidad inicial en Y
+let gravedad = 0.4;
 
-    // Aquí iría el resto de la lógica y dibujo del juego
+//plataformas
+let plataformas = [];
+let anchoPlataforma = 60;
+let altoPlataforma = 18;
+let plataformaImg;
 
-    requestAnimationFrame(gameLoop);
+let puntaje = 0;
+let puntajeMaximo = 0;
+let juegoTerminado = false;
+
+window.onload = function() {
+    tablero = document.getElementById("tablero");
+    tablero.height = altoTablero;
+    tablero.width = anchoTablero;
+    contexto = tablero.getContext("2d"); //escritura en el tablero
+
+    //dibuja el doodler
+    // contexto.fillStyle = "verde";
+    // contexto.fillRect(doodler.x, doodler.y, doodler.ancho, doodler.alto);
+
+    //carga la imagen
+    doodlerImgDerecha = new Image();
+    doodlerImgDerecha.src = "./doodler-right.png";
+    doodler.img = doodlerImgDerecha;
+    doodlerImgDerecha.onload = function() {
+        contexto.drawImage(doodler.img, doodler.x, doodler.y, doodler.ancho, doodler.alto);
+    }
+
+    doodlerImgIzquierda = new Image();
+    doodlerImgIzquierda.src = "./doodler-left.png";
+
+    plataformaImg = new Image();
+    plataformaImg.src = "./platform.png";
+
+    velocidadY = velocidadInicialY;
+    colocarPlataformas();
+    requestAnimationFrame(actualizar);
+    document.addEventListener("keydown", moverDoodler);
 }
-
-// Iniciar el juego después de que la imagen de fondo se haya cargado
-backgroundSprite.onload = function() {
-    gameLoop();
-};
